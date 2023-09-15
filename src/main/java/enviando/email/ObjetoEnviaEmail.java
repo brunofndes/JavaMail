3bf1e26d43eee5cb5cf5be6b9aacbdbced7f353d
Bruno Fernandes <brunof900@gmail.com>
2023-09-15 02:25:44 -0300

    transformando o AppTest numa classe isolada

---
+++ src/main/java/enviando/email/ObjetoEnviaEmail.java
@@ -0,0 +1,61 @@
+package enviando.email;
+
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class ObjetoEnviaEmail {
+
+	private String userName = "contatoveritasdev@gmail.com";
+	private String senha = "";
+
+	private String listaDeDestinatarios = "";
+	private String nomeRemetente = "";
+	private String assuntoEmail = "";
+	private String textoEmail = "";
+
+	public ObjetoEnviaEmail(String listaDeDestinatario, String nomeRemetente, String assuntoEmail, String textoEmail) {
+		this.listaDeDestinatarios = listaDeDestinatario;
+		this.assuntoEmail = assuntoEmail;
+		this.nomeRemetente = nomeRemetente;
+		this.textoEmail = textoEmail;
+	}
+
+	public void enviarEmail() throws Exception {
+
+		Properties properties = new Properties();
+
+		properties.put("mail.smtp.ssl.trust", "*");
+		properties.put("mail.smtp.auth", "true");/* Autorização */
+		properties.put("mail.smtp.starttls", "true");/* Autenticação */
+		properties.put("mail.smtp.host", "smtp.gmail.com");/* Servidor gmail */
+		properties.put("mail.smtp.port", "465");
+		properties.put("mail.smtp.socketFactory.port", "465");
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+		Session session = Session.getInstance(properties, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+
+				return new PasswordAuthentication(userName, senha);
+			}
+		});
+
+		Address[] toUser = InternetAddress.parse(listaDeDestinatarios);
+
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(userName, nomeRemetente));
+		message.setRecipients(Message.RecipientType.TO, toUser);
+		message.setSubject(assuntoEmail);
+		message.setText(textoEmail);
+
+		Transport.send(message);
+	}
+}
--- src/test/java/enviando/mail/AppTest.java
+++ src/test/java/enviando/mail/AppTest.java
@@ -11,47 +11,20 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-public class AppTest {
-	private String userName = "contatoveritasdev@gmail.com";
-	private String senha = "senhanaoexistente";
-	
-	@org.junit.Test
-	public void testeEmail(){
+import enviando.email.ObjetoEnviaEmail;
 
-	
+public class AppTest {
 		
-		try {
-			
-			Properties properties = new Properties();
-			properties.put("mail.smtp.auth", "true");/*Autorização*/
-			properties.put("mail.smtp.starttls", "true");/*Autenticação*/
-			properties.put("mail.smtp.host", "smtp.gmail.com");/*Servidor gmail*/
-			properties.put("mail.smtp.port", "465");
-			properties.put("mail.smtp.socketFactory.port", "465");
-			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-			
-			Session session = Session.getInstance(properties, new Authenticator() {
-				@Override
-				protected PasswordAuthentication getPasswordAuthentication() {
-					
-					return new PasswordAuthentication(userName, senha);
-				}
-			});
-			
-			Address[] toUser = InternetAddress.parse("brunof900@gmail.com, contatoveritasdev@gmail.com");
-			
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(userName));
-			message.setRecipients(Message.RecipientType.TO, toUser);
-			message.setSubject("Segundo email ");
-			message.setText("comprovação do teste agora");
-			
-			Transport.send(message);
-			
-		} catch (Exception e) {
-			
-			e.printStackTrace();
-		}
+	@org.junit.Test
+	public void testeEmail() throws Exception{	
+		
+		ObjetoEnviaEmail enviaEmail = new ObjetoEnviaEmail("brunof900@gmail.com",
+															"Bruno Fernandes",
+															"Email Refatorado",
+															"Corpo do email refatorado");
+		
+		enviaEmail.enviarEmail();
+		
 		
 	}
 }
